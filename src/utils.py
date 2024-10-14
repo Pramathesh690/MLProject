7b9b49fb532b6306cd5fa58fe40ba3c3c9bd10dc
Pramathesh690 <pramatheshts025@gmail.com>
2024-10-14 19:30:06 +0530

    final commit

--- src/utils.py
+++ src/utils.py
@@ -29,7 +29,7 @@ def evaluate_model(X_train , y_train , X_test , y_test , models , params):
             
             gs = GridSearchCV(model,para,cv=3,n_jobs=-1,verbose=False,refit=True)
             gs.fit(X_train,y_train)
-            
+
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
             y_train_pred = model.predict(X_train)
@@ -43,5 +43,14 @@ def evaluate_model(X_train , y_train , X_test , y_test , models , params):
             report[list(models.keys())[i]] = test_model_score
         
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+
+def load_object(file_path):
+    try:
+        with open(file_path , "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
